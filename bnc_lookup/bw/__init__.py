7b050019eab1406341160d084d65c14a24ebd93b
Craig Trim <craigtrim@gmail.com>
2026-01-23 20:08:52 -0600

    feat: Add words() and sample() for bucket-to-words lookup

---
+++ bnc_lookup/bw/__init__.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# AUTO-GENERATED - DO NOT MODIFY
+
+from .bw_01 import words_01
+from .bw_02 import words_02
+from .bw_03 import words_03
+from .bw_04 import words_04
+from .bw_05 import words_05
+from .bw_06 import words_06
+from .bw_07 import words_07
+from .bw_08 import words_08
+from .bw_09 import words_09
+from .bw_10 import words_10
+from .bw_11 import words_11
+from .bw_12 import words_12
+from .bw_13 import words_13
+from .bw_14 import words_14
+from .bw_15 import words_15
+from .bw_16 import words_16
+from .bw_17 import words_17
+from .bw_18 import words_18
+from .bw_19 import words_19
+from .bw_20 import words_20
+from .bw_21 import words_21
+from .bw_22 import words_22
+from .bw_23 import words_23
+from .bw_24 import words_24
+from .bw_25 import words_25
+from .bw_26 import words_26
+from .bw_27 import words_27
+from .bw_28 import words_28
+from .bw_29 import words_29
+from .bw_30 import words_30
+from .bw_31 import words_31
+from .bw_32 import words_32
+from .bw_33 import words_33
+from .bw_34 import words_34
+from .bw_35 import words_35
+from .bw_36 import words_36
+from .bw_37 import words_37
+from .bw_38 import words_38
+from .bw_39 import words_39
+from .bw_40 import words_40
+from .bw_41 import words_41
+from .bw_42 import words_42
+from .bw_43 import words_43
+from .bw_44 import words_44
+from .bw_45 import words_45
+from .bw_46 import words_46
+from .bw_47 import words_47
+from .bw_48 import words_48
+from .bw_49 import words_49
+from .bw_50 import words_50
+from .bw_51 import words_51
+from .bw_52 import words_52
+from .bw_53 import words_53
+from .bw_54 import words_54
+from .bw_55 import words_55
+from .bw_56 import words_56
+from .bw_57 import words_57
+from .bw_58 import words_58
+from .bw_59 import words_59
+from .bw_60 import words_60
+from .bw_61 import words_61
+from .bw_62 import words_62
+from .bw_63 import words_63
+from .bw_64 import words_64
+from .bw_65 import words_65
+from .bw_66 import words_66
+from .bw_67 import words_67
+from .bw_68 import words_68
+from .bw_69 import words_69
+from .bw_70 import words_70
+from .bw_71 import words_71
+from .bw_72 import words_72
+from .bw_73 import words_73
+from .bw_74 import words_74
+from .bw_75 import words_75
+from .bw_76 import words_76
+from .bw_77 import words_77
+from .bw_78 import words_78
+from .bw_79 import words_79
+from .bw_80 import words_80
+from .bw_81 import words_81
+from .bw_82 import words_82
+from .bw_83 import words_83
+from .bw_84 import words_84
+from .bw_85 import words_85
+from .bw_86 import words_86
+from .bw_87 import words_87
+from .bw_88 import words_88
+from .bw_89 import words_89
+from .bw_90 import words_90
+from .bw_91 import words_91
+from .bw_92 import words_92
+from .bw_93 import words_93
+from .bw_94 import words_94
+from .bw_95 import words_95
+from .bw_96 import words_96
+from .bw_97 import words_97
+from .bw_98 import words_98
+from .bw_99 import words_99
+from .bw_100 import words_100
+
+__all__ = [
+    'words_01',
+    'words_02',
+    'words_03',
+    'words_04',
+    'words_05',
+    'words_06',
+    'words_07',
+    'words_08',
+    'words_09',
+    'words_10',
+    'words_11',
+    'words_12',
+    'words_13',
+    'words_14',
+    'words_15',
+    'words_16',
+    'words_17',
+    'words_18',
+    'words_19',
+    'words_20',
+    'words_21',
+    'words_22',
+    'words_23',
+    'words_24',
+    'words_25',
+    'words_26',
+    'words_27',
+    'words_28',
+    'words_29',
+    'words_30',
+    'words_31',
+    'words_32',
+    'words_33',
+    'words_34',
+    'words_35',
+    'words_36',
+    'words_37',
+    'words_38',
+    'words_39',
+    'words_40',
+    'words_41',
+    'words_42',
+    'words_43',
+    'words_44',
+    'words_45',
+    'words_46',
+    'words_47',
+    'words_48',
+    'words_49',
+    'words_50',
+    'words_51',
+    'words_52',
+    'words_53',
+    'words_54',
+    'words_55',
+    'words_56',
+    'words_57',
+    'words_58',
+    'words_59',
+    'words_60',
+    'words_61',
+    'words_62',
+    'words_63',
+    'words_64',
+    'words_65',
+    'words_66',
+    'words_67',
+    'words_68',
+    'words_69',
+    'words_70',
+    'words_71',
+    'words_72',
+    'words_73',
+    'words_74',
+    'words_75',
+    'words_76',
+    'words_77',
+    'words_78',
+    'words_79',
+    'words_80',
+    'words_81',
+    'words_82',
+    'words_83',
+    'words_84',
+    'words_85',
+    'words_86',
+    'words_87',
+    'words_88',
+    'words_89',
+    'words_90',
+    'words_91',
+    'words_92',
+    'words_93',
+    'words_94',
+    'words_95',
+    'words_96',
+    'words_97',
+    'words_98',
+    'words_99',
+    'words_100',
+]
